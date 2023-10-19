ce35315832f4e05c9a6444b0ff3241bb6b158e68
paulbhaskar <97970917+paulbhaskar@users.noreply.github.com>
2023-10-19 02:08:42 -0400

    cors whitelisted port 3000

--- config/settings.py
+++ config/settings.py
@@ -41,12 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'monte_carlo_simulation'
 ]
 
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1", "http://localhost",
+                        "http://127.0.0.1:3000", "http://localhost:3000"]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
